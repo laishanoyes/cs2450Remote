9bec2b135e84d58990cd89916389eea60f21ddc7
laishanoyes <d00447258@utahtech.edu>
2026-01-20 10:12:34 -0700

    added a modifier so it did not guess the same age multiple times.

--- Age_guesser.py
+++ Age_guesser.py
@@ -3,8 +3,11 @@ print("Hello, I am going to try to guess your age.")
 name = input("What is your name?")
 print("Welcome" + name)
 
+ages = list(range(15,31))
+
 while True:
-	age_guess = random.randint(15,30)
+	age_guess = random.choice(ages)
+	ages.remove(age_guess)
 	answer = input(f"Is your age {age_guess}? (y/n): ").lower()
 
 	if answer == 'y':
