35d621509ef5dc97bbc30ed9bd0db122387ee2bd
laishanoyes <d00447258@utahtech.edu>
2026-01-25 23:00:54 -0700

    added the ability to tell the guesser older or younger

--- Age_guesser.py
+++ Age_guesser.py
@@ -1,19 +1,34 @@
 import random
 print("Hello, I am going to try to guess your age.")
-name = input("What is your name?")
+name = input("What is your name? ")
 print("Welcome" + name)
 
 ages = list(range(15,31))
 
-while True:
+while ages:
 	age_guess = random.choice(ages)
-	ages.remove(age_guess)
-	answer = input(f"Is your age {age_guess}? (y/n): ").lower()
+	answer = input(
+	f"Is your age {age_guess}? (y = yes, o = older, u = younger): " ).lower()
+
 
 	if answer == 'y':
-		print(f"Hooray! {name} is {age_guess} years old. ")
+		print(f"Hooray! {name} is {age_guess} years old.")
 		break
 
-	else:
-		print("Dang it, I will try again")
 
+	elif answer == 'o':
+		ages = [age for age in ages if age > age_guess]
+		print("Dang it, I'll guess older.")
+
+
+	elif answer == 'u': 
+		ages = [age for age in ages if age < age_guess]
+		print("Dang it, I'll guess younger.")
+
+
+	else:
+		print("Please enter y, o, or u.")
+
+
+else:
+	print("Hmm... something doesn't add up!")
